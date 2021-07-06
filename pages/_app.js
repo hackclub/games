fce4ca65a79a205be7c7d94406426ac2af242d59
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-07-06 21:25:54 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -56,6 +56,7 @@ export default class App extends NextApp {
           <Meta
             name="Hack Club"
             title="The Hacker Games"
+            image="https://games.hackclub.dev/hacker_games_social_image.png"
             description={`As we rise out of the COVID-19 pandemic, we're bringing the Hacker World together to participate in the Hacker Games. We'll have bucket loads of fun, make new friends and participate in a hacking pentathlon over the week. All over Zoom & Slack from July 17th to July 22nd.`}
           />
           <title>The Hacker Games by Hack Club</title>
