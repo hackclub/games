8a37c2f647be796c4406046d5ddd33af01e2fd73
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-07-07 17:33:11 +0800

    Add more!

--- pages/api/sign-up.js
+++ pages/api/sign-up.js
@@ -1,6 +1,14 @@
 export default async (req, res) => {
-  console.log(req.query)
+
   const { WebClient } = require('@slack/web-api')
+
+  const AirtablePlus = require('airtable-plus')
+
+  const airtable = new AirtablePlus({
+    baseID: 'appM2tajzzNei6tYe',
+    apiKey: process.env.AIRTABLE_TICKETS_TOKEN,
+    tableName: 'Participants'
+  })
 
   const token = process.env.SLACK_TOKEN
 
@@ -9,5 +17,9 @@ export default async (req, res) => {
   let details = await web.users.info({
     user: req.query.username
   })
+
+  let registration = await airtable.create({'Name': details.user.real_name, 'Email': details.user.profile.email, 'Slack ID': details.user.id, 'Pronouns': details.user.profile.pronouns})
+
+  res.json(registration)
 
 }
